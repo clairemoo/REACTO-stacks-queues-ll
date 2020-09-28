5db1c8ae4cd14240c0ab71c2b93ef4feab7a78fd
Michael McInerney <michael.mcinerney@fullstackacademy.com>
2020-09-28 09:28:30 -0400

    Fix nextGreatestElement

--- nextGreatestElement.js
+++ nextGreatestElement.js
@@ -3,16 +3,16 @@ function nextGreatestElement(input) {
   const nextGreatestArray = Array(input.length).fill(-1, 0, input.length), stack=[];
   for (let i = 0; i < input.length; i++) {
     const elem = input[i];
+    console.log('elem', elem);
+    console.log('stack', stack);
     // Get our top element index, so we can use it later without removing. stack.peek()
-    let stackIndex = stack.length - 1;
-    while(stackIndex >= 0) {
+    while(stack.length - 1 >= 0) {
       // Get the previous index so we can check if we currently are at a greater value
-      const previousIndex = stack[stackIndex];
-      if(input[previousIndex] < elem) {
+      const previousArrayIndex = stack[stack.length - 1];
+      if(input[previousArrayIndex] < elem) {
         // Now that we have found a solution, we need to save it and backtrack
-        nextGreatestArray[previousIndex] = elem;
+        nextGreatestArray[previousArrayIndex] = elem;
         stack.pop();
-        stackIndex--;
       } else {
         break;
       }
@@ -22,10 +22,11 @@ function nextGreatestElement(input) {
   return nextGreatestArray;
 }
 
-const array = [4, 5, 2, 25];
 
-const array2 = [13, 7, 6, 12];
+const array = [4, 5, 2, 25];
 
 console.log(nextGreatestElement(array));
 
-console.log(nextGreatestElement(array2));
+// const array2 = [13, 7, 6, 12];
+
+// console.log(nextGreatestElement(array2));
